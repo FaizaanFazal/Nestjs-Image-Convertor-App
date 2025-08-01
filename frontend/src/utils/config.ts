5a096c8c546f83195b5df742fa875e28d2d6a04b
FaizaanFazal <dkfaizaan12@gmail.com>
2025-08-01 14:18:52 +0900

    update: tested backend

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,6 +1,6 @@
 // src/config.ts
 export const API_URL =
-  import.meta.env.VITE_BACKEND_URL || "/api";
+  import.meta.env.VITE_BACKEND_URL+"/api" || "/api";
 
 export const SOCKET_URL =
   import.meta.env.VITE_BACKEND_WS_URL ||
