45a02510f46ad2daea2678749bda0be2326a59cb
FaizaanFazal <dkfaizaan12@gmail.com>
2025-08-01 14:01:14 +0900

    update: cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://nestjs-image-convertor-24weyb5jl-faizaanfazals-projects.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
